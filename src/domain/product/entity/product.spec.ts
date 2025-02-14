c2559cbbba576829c1df53298aa35a7ac0586f69
Everton Rocha <evertonrps@gmail.com>
2025-02-14 16:40:01 -0300

    Added Notification to Product

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -1,23 +1,6 @@
 import Product from "./product";
 
 describe("Product unit tests", () => {
-  it("should throw error when id is empty", () => {
-    expect(() => {
-      const product = new Product("", "Product 1", 100);
-    }).toThrowError("Id is required");
-  });
-
-  it("should throw error when name is empty", () => {
-    expect(() => {
-      const product = new Product("123", "", 100);
-    }).toThrowError("Name is required");
-  });
-
-  it("should throw error when price is less than zero", () => {
-    expect(() => {
-      const product = new Product("123", "Name", -1);
-    }).toThrowError("Price must be greater than zero");
-  });
 
   it("should change name", () => {
     const product = new Product("123", "Product 1", 100);
@@ -29,5 +12,29 @@ describe("Product unit tests", () => {
     const product = new Product("123", "Product 1", 100);
     product.changePrice(150);
     expect(product.price).toBe(150);
+  });
+
+  it("should throw error when id is empty", () => {
+    expect(() => {
+      let product = new Product("", "Product 1", 100);
+    }).toThrowError("Id is required");
+  });
+
+  it("should throw error when name is empty", () => {
+    expect(() => {
+      let product = new Product("123", "", 100);
+    }).toThrowError("product: Name is required");
+  });
+
+  it("should throw error when price is less than zero", () => {
+    expect(() => {
+      let product = new Product("123", "Name", -1);
+    }).toThrowError("product: Price must be greater than zero");
+  });
+
+  it("should throw error when price is less than zero and name is empty", () => {
+    expect(() => {
+      let product = new Product("", "Name", -1);
+    }).toThrowError("product: Id is required,product: Price must be greater than zero");
   });
 });
